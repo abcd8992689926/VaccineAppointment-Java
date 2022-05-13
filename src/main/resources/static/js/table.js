2a9631217c8b02aca396b80de4b1fa542cf48efb
LAI <abcd8992689926@gmail.com>
2022-05-13 23:27:09 +0800

    login page and title update

--- src/main/resources/static/js/table.js
+++ src/main/resources/static/js/table.js
@@ -1,7 +1,10 @@
-function add(respone, saints){
+function add(respone, saints, even){
     let table=document.getElementById("data");
     for(var i=0; i<respone.length; i++){
         let newRow=table.insertRow();
+        if(i%2==0) {
+			newRow.className="dark";
+		}
         let cellTitle=newRow.insertCell();
         let cellValue=newRow.insertCell();
         cellTitle.innerHTML=saints[i];
